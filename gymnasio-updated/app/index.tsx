72ac777843c902be143b0c361511bfdd1a0f51bf
Ayush Bansal <bansalayush.69@gmail.com>
2026-05-08 01:01:24 +0530

    + default route sorted

--- gymnasio-updated/app/index.tsx
+++ gymnasio-updated/app/index.tsx
@@ -1,19 +1,5 @@
-import { View, Button } from "react-native";
-import { router } from "expo-router";
+import { Redirect } from "expo-router";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Button
-        title="Go to Onboarding"
-        onPress={() => router.push("/onboarding")}
-      />
-    </View>
-  );
+  return <Redirect href="/onboarding" />;
 }
